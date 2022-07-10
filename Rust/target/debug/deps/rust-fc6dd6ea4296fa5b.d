ba901d1dae1a0b3f9e4cf10b682bac011e747a2d
kana-rus <kanarus786@gmail.com>
2022-07-10 19:12:52 +0900

    removed target/ from git

--- Rust/target/debug/deps/rust-fc6dd6ea4296fa5b.d
+++
@@ -1,5 +0,0 @@
-/home/kanarus/learn_compiler/Rust/target/debug/deps/rust-fc6dd6ea4296fa5b.rmeta: src/main.rs
-
-/home/kanarus/learn_compiler/Rust/target/debug/deps/rust-fc6dd6ea4296fa5b.d: src/main.rs
-
-src/main.rs:
--- Rust/target/debug/rust.d
+++
@@ -1,1 +0,0 @@
-/home/kanarus/learn_compiler/Rust/target/debug/rust: /home/kanarus/learn_compiler/Rust/src/main.rs
