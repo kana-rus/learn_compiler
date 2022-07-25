4e30b39107934de14185a340ef0616a6f87fde67
kana-rus <kanarus786@gmail.com>
2022-07-25 22:45:15 +0900

    Parsed: expr ::= mul('+'|'-' mul)* // mul ::= prim('*'|'/' prim)* // prim ::= num | '('expr')'

--- src/004.rs
+++ src/004.rs
@@ -11,22 +11,12 @@ impl Parse for Arith {
     fn parse(mut tokens: std::collections::vec_deque::IntoIter<Token>) -> Tree {
         let mut arith = Arith { current: /*Token::Init*/tokens.next().expect("no input given") };
         let /*mut*/ root = expr(&mut arith, &mut tokens);
-        /*
-        while root.elem.is_init() && root.right.is_none() {
-            root = *root.left.unwrap();
-        }
-        */
         Tree { root }
     }
 }
 
 fn expr(arith: &mut Arith, tokens: &mut IntoIter<Token>) -> Node {
-//    println!("[arith] expr1 {:?}", arith.current);//
     let mut node = mul(arith, tokens);
-//    println!("[node] expr1 {:?}", &node);
-    //let next = tokens.next(); if next.is_none() { return node; }
-    //arith.current = next.unwrap();
-//    println!("[arith] expr2 {:?}", arith.current);//
     loop {
         match arith.current {
             Token::Ope('+') => {
@@ -51,12 +41,7 @@ fn expr(arith: &mut Arith, tokens: &mut IntoIter<Token>) -> Node {
 }
 
 fn mul(arith: &mut Arith, tokens: &mut IntoIter<Token>) -> Node {
-//    println!("[arith] mul1 {:?}", arith.current);//
     let mut node = prim(arith, tokens);
-//    println!("[node] mul1 {:?}", &node);
-//    let next = tokens.next(); if next.is_none() { return node; }
-//    arith.current = next.unwrap();
-//    println!("[arith] mul2 {:?}", arith.current);//
     loop {
         match arith.current {
             Token::Ope('*') => {
@@ -95,8 +80,6 @@ fn prim(arith: &mut Arith, tokens: &mut IntoIter<Token>) -> Node {
         Token::PrimOpen => {
             arith.current = tokens.next().expect("no \")\"");
             let node = expr(arith, tokens);
-//    println!("[node] prim1 {:?}", &node);
-//    println!("[arith] prim2 {:?}", arith.current);//
             
             if !arith.current.is_prim_close() {
                 exit_with_report("no \")\"");
@@ -104,7 +87,6 @@ fn prim(arith: &mut Arith, tokens: &mut IntoIter<Token>) -> Node {
             if let Some(next) = tokens.next() {
                 arith.current = next;
             }
-            // if tokens.next().expect("no close").expect_ope() != ')' { exit_with_report("no \")\" found"); }
             return node;
         },
 
